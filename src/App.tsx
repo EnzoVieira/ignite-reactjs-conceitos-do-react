0ba1bf8b69fb462ad824435c0839a39f1a5cb520
Enzo Vieira <enzovieira16@hotmail.com>
2022-04-08 12:05:44 +0100

    Finish challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { TaskList } from './components/TaskList'
-import { Header } from "./components/Header";
-import './styles/global.scss'
-
+import { TaskList } from "./components/TaskList"
+import { Header } from "./components/Header"
+import "./styles/global.scss"
 
 export function App() {
   return (
